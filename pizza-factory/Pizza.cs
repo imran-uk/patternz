9afbc34103f92af7350a25fc926f95987b5b9122
Imran Chaudhry <imran.chaudhry@stepstone.co.uk>
2023-05-02 09:18:34 +0100

    Pizza Factory revamped

---
+++ pizza-factory/Pizza.cs
@@ -0,0 +1,26 @@
+﻿namespace patternz;
+
+class Pizza : IPizza
+{
+    private readonly List<string> _ingredients;
+
+    public Pizza(List<string> ingredients)
+    {
+        _ingredients = ingredients;
+    }
+
+    public void Bake()
+    {
+        Console.WriteLine($"baking the {string.Join(",", _ingredients)}");
+    }
+
+    public void PutInBox()
+    {
+        Console.WriteLine($"puttin the {string.Join(",", _ingredients)} in da box");
+    }
+
+    public void Deliver()
+    {
+        Console.WriteLine($"deliver the {string.Join(",", _ingredients)}");
+    }
+}
